a61a29ec799a787695d59b22556f3ec72e136a5a
Plínio Pvv <gangss2@hotmail.com>
2024-05-11 09:54:21 -0300

    calculo_area

---
+++ calculo_area/src/calculo_area/CalculaArea.java
@@ -0,0 +1,19 @@
+package calculo_area;
+
+import java.util.Scanner;
+
+public class CalculaArea {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Digite os parâmetros da área à calcular:");
+		System.out.print(" X = ");
+		double x = in.nextDouble();
+		System.out.print(" Y = ");
+		double y = in.nextDouble();
+		in.close();
+		
+		System.out.println("Área total: \n\t\t" + x*y + " m²");
+		
+	}
+}
