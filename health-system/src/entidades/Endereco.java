7fa492714ccb1dc606d549e40325c327d4126285
Plínio Pvv <gangss2@hotmail.com>
2024-05-25 11:01:37 -0300

    +Endereco

---
+++ health-system/src/entidades/Endereco.java
@@ -0,0 +1,65 @@
+package entidades;
+
+//[M2S03] Ex. 02 - Sistema de Saúde Parte 2 - Endereço
+public class Endereco {
+	
+	private String logradouro;
+	private String Estado;
+	private String Cidade;
+	private int Numero;
+	private int cep;
+	
+	public Endereco() {
+		
+	}
+	
+	public Endereco(String logradouro, String estado, String cidade, int numero, int cep) {
+		super();
+		this.logradouro = logradouro;
+		Estado = estado;
+		Cidade = cidade;
+		Numero = numero;
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getEstado() {
+		return Estado;
+	}
+
+	public void setEstado(String estado) {
+		Estado = estado;
+	}
+
+	public String getCidade() {
+		return Cidade;
+	}
+
+	public void setCidade(String cidade) {
+		Cidade = cidade;
+	}
+
+	public int getNumero() {
+		return Numero;
+	}
+
+	public void setNumero(int numero) {
+		Numero = numero;
+	}
+
+	public int getCep() {
+		return cep;
+	}
+
+	public void setCep(int cep) {
+		this.cep = cep;
+	}
+
+}
