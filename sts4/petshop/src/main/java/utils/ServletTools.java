27f165a75355b6d8d6c3eaf117c39306f4ac38c9
Plínio Pvv <gangss2@hotmail.com>
2024-06-22 12:22:44 -0300

    clean

--- sts4/petshop/src/main/java/utils/ServletTools.java
+++ sts4/petshop/src/main/java/utils/ServletTools.java
@@ -10,23 +10,28 @@ import com.google.gson.Gson;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Pet;
 import model.PetShopResponse;
 
 public abstract class ServletTools {
-	
+
 	public static void send(HttpServletResponse resp, String msg) throws IOException {
-		resp.getWriter().append("<html><p>"+ msg +"</p></html>");
+		resp.getWriter().append("<html><p>" + msg + "</p></html>");
 	}
-	
+
 	public static void send(HttpServletResponse resp, List<? extends PetShopResponse> list) throws IOException {
 		final StringBuilder sb = new StringBuilder();
 		sb.append("<html>");
 		sb.append("<p>");
+
+		for (int i = 0; i < list.size(); i++) {
+			PetShopResponse x = list.get(i);
+			sb.append("<p>" + i + " - " + x.toString() + "</p>");
+		}
+
 		list.forEach(new Consumer<PetShopResponse>() {
 			@Override
 			public void accept(PetShopResponse pet) {
-				sb.append("<p>"+pet.toString()+"</p>");
+
 			}
 		});
 		sb.append("</p>");
@@ -38,12 +43,12 @@ public abstract class ServletTools {
 		BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
 		String line = "";
 		StringBuilder sb = new StringBuilder();
-		while((line = br.readLine()) != null) {
+		while ((line = br.readLine()) != null) {
 			sb.append(line);
 		}
-		
+
 		System.out.println(sb.toString());
-	
+
 		Gson gson = new Gson();
 		return gson.fromJson(sb.toString(), classs);
 	}
