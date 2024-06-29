4b6584746852e07bc9f8782795c65878f86e45f8
Plínio Pvv <gangss2@hotmail.com>
2024-06-29 13:14:52 -0300

    .

--- sts4/springsaude/src/main/java/br/pvv/senai/saude/repository/ConsultaRepository.java
+++ sts4/springsaude/src/main/java/br/pvv/senai/saude/repository/ConsultaRepository.java
@@ -2,8 +2,8 @@ package br.pvv.senai.saude.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.pvv.senai.saude.model.Nutricionista;
+import br.pvv.senai.saude.model.Consulta;
 
-public interface ConsultaRepository extends JpaRepository<Nutricionista, Long>  {
+public interface ConsultaRepository extends JpaRepository<Consulta, Long>  {
 
 }
