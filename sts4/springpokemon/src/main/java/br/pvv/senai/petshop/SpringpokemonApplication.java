d0ed396260479b29510075fc4b470a121781a8ff
Plínio Pvv <gangss2@hotmail.com>
2024-07-06 10:59:00 -0300

    +springpokemon

---
+++ sts4/springpokemon/src/main/java/br/pvv/senai/petshop/SpringpokemonApplication.java
@@ -0,0 +1,13 @@
+package br.pvv.senai.petshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpokemonApplication.class, args);
+	}
+
+}
