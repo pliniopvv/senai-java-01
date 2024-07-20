3e641d588d0609f07efb324d5d921a9bd68151ea
Plínio Pvv <gangss2@hotmail.com>
2024-07-20 11:54:34 -0300

    +config

---
+++ sts4/springcadmedic/src/main/java/br/pvv/senai/SpringcadmedicApplication.java
@@ -0,0 +1,13 @@
+package br.pvv.senai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcadmedicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcadmedicApplication.class, args);
+	}
+
+}
---
+++ sts4/springcadmedic/src/test/java/br/pvv/senai/SpringcadmedicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.pvv.senai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcadmedicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
