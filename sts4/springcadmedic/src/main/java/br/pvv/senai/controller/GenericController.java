f9ea0cc2e283cfc47fde2bade3f9f1ae31eeb9fc
Plínio Pvv <gangss2@hotmail.com>
2024-07-20 12:39:17 -0300

    +medicoEntity/fixes

--- sts4/springcadmedic/src/main/java/br/pvv/senai/controller/GenericController.java
+++ sts4/springcadmedic/src/main/java/br/pvv/senai/controller/GenericController.java
@@ -5,22 +5,27 @@ import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.pvv.senai.model.IEntity;
 import br.pvv.senai.service.GenericService;
 
+@RestController
 public abstract class GenericController<T extends IEntity> {
 
 	public abstract GenericService<T> getService();
 
 	@GetMapping("{id}")
-	public ResponseEntity<T> get(long id) {
+	public ResponseEntity<T> get(@PathVariable long id) {
 		Optional<T> model = getService().load(id);
-		if (model.isEmpty()) return ResponseEntity.notFound().build();
-		else return ResponseEntity.ok(model.get());
+		if (model.isEmpty())
+			return ResponseEntity.notFound().build();
+		else
+			return ResponseEntity.ok(model.get());
 	}
 
 	@PostMapping
@@ -29,15 +34,16 @@ public abstract class GenericController<T extends IEntity> {
 	}
 
 	@PutMapping("{id}")
-	public T put(long id, @RequestBody T model) {
+	public T put(@PathVariable long id, @RequestBody T model) {
 		model.setId(id);
-		return getService().alter(model)id;
+		return getService().alter(model);
 	}
 
 	@DeleteMapping("{id}")
-	public ResponseEntity del(long id) {
+	public ResponseEntity del(@PathVariable long id) {
 		Optional<T> model = getService().load(id);
-		if (model.isPresent()) getService().delete(model);
+		if (model.isPresent())
+			getService().delete(model.get());
 		return ResponseEntity.noContent().build();
 	}
 }
