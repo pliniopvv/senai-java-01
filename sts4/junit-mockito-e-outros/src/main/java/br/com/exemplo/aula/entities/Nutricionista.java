375702cc52726f34249d7a5407fd6a647e94f224
Plínio Pvv <gangss2@hotmail.com>
2024-09-21 17:21:08 -0300

    +NutricionistaServiceTest

--- sts4/junit-mockito-e-outros/src/main/java/br/com/exemplo/aula/entities/Nutricionista.java
+++ sts4/junit-mockito-e-outros/src/main/java/br/com/exemplo/aula/entities/Nutricionista.java
@@ -1,88 +1,108 @@
 package br.com.exemplo.aula.entities;
 
-import jakarta.persistence.*;
-
 import java.util.HashSet;
 import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "nutricionista")
 public class Nutricionista {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_nutricionista")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_nutricionista")
+	private Long id;
 
-    @Column(nullable = false)
-    private String nome;
+	@Column(nullable = false)
+	private String nome;
 
-    private String matricula;
-    private int tempoExperiencia;
+	private String matricula;
+	private int tempoExperiencia;
 
-    @Column(nullable = false)
-    private String crn;
+	@Column(nullable = false)
+	private String crn;
 
-    private String especialidade;
+	private String especialidade;
 
-    @ElementCollection
-    private Set<String> certificacoes = new HashSet<>();
+	@ElementCollection
+	private Set<String> certificacoes = new HashSet<>();
 
+	public Nutricionista() {
+		super();
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Nutricionista(Long id, String nome, String matricula, int tempoExperiencia, String crn, String especialidade,
+			Set<String> certificacoes) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.matricula = matricula;
+		this.tempoExperiencia = tempoExperiencia;
+		this.crn = crn;
+		this.especialidade = especialidade;
+		this.certificacoes = certificacoes;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public String getMatricula() {
-        return matricula;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setMatricula(String matricula) {
-        this.matricula = matricula;
-    }
+	public String getMatricula() {
+		return matricula;
+	}
 
-    public int getTempoExperiencia() {
-        return tempoExperiencia;
-    }
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
 
-    public void setTempoExperiencia(int tempoExperiencia) {
-        this.tempoExperiencia = tempoExperiencia;
-    }
+	public int getTempoExperiencia() {
+		return tempoExperiencia;
+	}
 
+	public void setTempoExperiencia(int tempoExperiencia) {
+		this.tempoExperiencia = tempoExperiencia;
+	}
 
-    public String getCrn() {
-        return crn;
-    }
+	public String getCrn() {
+		return crn;
+	}
 
-    public void setCrn(String crn) {
-        this.crn = crn;
-    }
+	public void setCrn(String crn) {
+		this.crn = crn;
+	}
 
-    public String getEspecialidade() {
-        return especialidade;
-    }
+	public String getEspecialidade() {
+		return especialidade;
+	}
 
-    public void setEspecialidade(String especialidade) {
-        this.especialidade = especialidade;
-    }
+	public void setEspecialidade(String especialidade) {
+		this.especialidade = especialidade;
+	}
 
-    public Set<String> getCertificacoes() {
-        return certificacoes;
-    }
+	public Set<String> getCertificacoes() {
+		return certificacoes;
+	}
 
-    public void setCertificacoes(Set<String> certificacoes) {
-        this.certificacoes = certificacoes;
-    }
+	public void setCertificacoes(Set<String> certificacoes) {
+		this.certificacoes = certificacoes;
+	}
 }
---
+++ sts4/junit-mockito-e-outros/src/test/java/br/com/exemplo/aula/service/NutricionistaServiceTest.java
@@ -0,0 +1,101 @@
+package br.com.exemplo.aula.service;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.exemplo.aula.controllers.dto.NutricionistaRequestDTO;
+import br.com.exemplo.aula.entities.Nutricionista;
+import br.com.exemplo.aula.repositories.NutricionistaRepository;
+import br.com.exemplo.aula.services.NutricionistaService;
+
+@ExtendWith(MockitoExtension.class)
+public class NutricionistaServiceTest {
+
+	@Mock
+	NutricionistaRepository repository;
+
+	@InjectMocks
+	NutricionistaService service;
+
+	Nutricionista nutricionista;
+
+	@BeforeEach
+	public void setup() {
+		nutricionista = new Nutricionista(1L, "Um nome de nutricionista", "12323", 5, "CRN12311", "Faxineira",
+				Set.of("Certificado 1", "Certificado 2"));
+	}
+
+	@Test
+	@DisplayName("Teste listar nutricionistas!")
+	void listarNutricionistas() {
+
+		var lista = new ArrayList<Nutricionista>();
+		lista.add(nutricionista);
+		when(repository.findAll()).thenReturn(lista);
+
+		var resultado = service.listarNutricionistas();
+
+		assertNotNull(resultado);
+		assertEquals(resultado.get(0).getId(), lista.get(0).getId());
+
+		verify(repository).findAll();
+	}
+
+	@Test
+	@DisplayName("Teste buscar nutricionista!")
+	void buscarNutricionista() {
+		
+		when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(nutricionista));
+		
+		var resultado = service.buscarNutricionista(1L);
+		
+		assertNotNull(resultado);
+		assertEquals(nutricionista.getId(), resultado.getId());
+		
+		verify(repository).findById(anyLong());
+	}
+
+	@Test
+	@DisplayName("Teste salvar nutricionista!")
+	void salvarNutricionista() {
+		NutricionistaRequestDTO entity = new NutricionistaRequestDTO("Um nome de nutricionista", "123123", 5, 12L,
+				"CRN12311", "Faxineira");
+		;
+
+		when(repository.save(any())).thenReturn(nutricionista);
+
+		var resultado = service.salvarNutricionista(entity);
+
+		assertNotNull(resultado);
+		assertEquals(resultado.getId(), nutricionista.getId());
+	}
+
+	@Test
+	@DisplayName("Teste remover nutricionista!")
+	void removerNutricionista() {
+		doNothing().when(repository).deleteById(anyLong());
+
+		assertDoesNotThrow(() -> service.removerNutricionista(1L));
+
+		verify(repository).deleteById(anyLong());
+	}
+
+}
